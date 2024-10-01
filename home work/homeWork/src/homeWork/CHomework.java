99a8b99abec1ba630880dd74e893fd6aa173b810
latifhaqjou <haqjoul@gmail.com>
2024-10-02 04:24:30 +0430

    Add files via upload

---
+++ home work/homeWork/src/homeWork/CHomework.java
@@ -0,0 +1,15 @@
+package homeWork;
+
+public class CHomework {
+	public static void main(String[] args) {
+		System.out.println("my name is Latif ");
+		System.out.println("my last name is Haqjou");
+		System.out.println("I am a student in KU");
+		System.out.println("My family live in balkhab Sar-e-pool");
+		System.out.println("but I live in kabul naw ");
+		System.out.println("I am 18 years old");
+		System.out.println("I want to be a hacker in the future");
+
+	}
+
+}
